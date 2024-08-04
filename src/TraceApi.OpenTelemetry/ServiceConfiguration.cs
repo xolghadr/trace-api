09197f7fe4398f4d6e6223892037ecdf7b06dc18
Sadegh Xolghadr <xolghadr@outlook.com>
2024-08-05 00:02:07 +0330

    first commit

---
+++ src/TraceApi.OpenTelemetry/ServiceConfiguration.cs
@@ -0,0 +1,18 @@
+﻿
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TraceApi.OpenTelemetry;
+public static class ServiceConfiguration
+{
+    public static void AddTelemetryTrace(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddOpenTelemetry()
+            .ConfigureResource(resource => resource
+                .AddService(serviceName: "builder.Environment.ApplicationName"))
+            .WithTracing(tracing => tracing
+                .AddAspNetCoreInstrumentation()
+                .AddConsoleExporter());
+    }
+}
